4b316df38cd54f53d0dbc2ba4130c0110e0326da
Chris Jewell <chrisjohnjewell@gmail.com>
2015-01-31 10:25:16 +1100

    Initial Commit

---
+++ PocketDrumLessonTests/PocketDrumLessonTests.m
@@ -0,0 +1,40 @@
+//
+//  PocketDrumLessonTests.m
+//  PocketDrumLessonTests
+//
+//  Created by Test Mac on 31/01/2015.
+//  Copyright (c) 2015 Chris Jewell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface PocketDrumLessonTests : XCTestCase
+
+@end
+
+@implementation PocketDrumLessonTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
